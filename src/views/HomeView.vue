68998baf9040d81564be1358c8970c6f76c2fa28
kmaurice012 <kingmaurice012@gmail.com>
2023-11-17 16:02:54 +0300

    get recently viewed items from cache

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,15 @@
     <div class="popular-recipes-container">
       <h2>Popular Recipes</h2>
       <div class="popular-recipes">
-        <Recipe 
-          scroll=true
+        <Recipe
+          scroll="true"
           v-for="recipe in popularRecipes"
           v-bind:key="recipe.id"
-          :id=recipe.id
-          :image=recipe.image
-          :name=recipe.title
-          :time=recipe.readyInMinutes
-          :serving=recipe.servings
+          :id="recipe.id"
+          :image="recipe.image"
+          :name="recipe.title"
+          :time="recipe.readyInMinutes"
+          :serving="recipe.servings"
         />
       </div>
     </div>
@@ -20,15 +20,15 @@
     <div class="recently-viewed-container" v-if="recentlyViewed">
       <h2>Recently viewed</h2>
       <div class="recently-viewed">
-        <Recipe 
+        <Recipe
           v-for="recipe in recentlyViewed"
-          :image=recipe.image
-          :id=recipe.id
+          :image="recipe.image"
+          :id="recipe.id"
           v-bind:key="recipe.id"
-          :name=recipe.title
+          :name="recipe.title"
           vertical="false"
-          :time=recipe.readyInMinutes
-          :serving=recipe.servings
+          :time="recipe.readyInMinutes"
+          :serving="recipe.servings"
         />
       </div>
     </div>
@@ -36,56 +36,61 @@
 </template>
 
 <script setup>
-import Hero from '../components/HeroComponent.vue'
-import Recipe from '../components/RecipeComponent.vue'
-import axios from 'axios';
-import {ref} from "vue"
+import Hero from "../components/HeroComponent.vue";
+import Recipe from "../components/RecipeComponent.vue";
+import axios from "axios";
+import { ref } from "vue";
 
-const popularRecipes = ref([])
-const recentlyViewed = JSON.parse(localStorage.getItem('recently-viewed'));
-
-(async() => {
-  const res = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?number=8&sort=popularity&addRecipeInformation=true&apiKey=${process.env.VUE_APP_API_KEY}`);
-  if (res?.data) popularRecipes.value = res.data.results
-})()
-
+const popularRecipes = ref([]);
+const recentlyViewed = ref([])(async () => {
+  if ("caches" in window) {
+    const cache = await caches.open("recently-viewed");
+    const responses = await cache.matchAll();
+    if (responses.length > 0) {
+      const responsesData = await Promise.all(
+        responses.map(async (response) => await response.json())
+      );
+      recentlyViewed.value = responsesData;
+    }
+  }
+})();
 </script>
 
 <style>
-  .popular-recipes-container {
-    margin-top: 30px;
-  }
+.popular-recipes-container {
+  margin-top: 30px;
+}
 
-  .popular-recipes-container h2 {
-    margin-bottom: 20px;
-  }
+.popular-recipes-container h2 {
+  margin-bottom: 20px;
+}
 
-  .popular-recipes {
-    overflow-x: scroll;
-    display: flex;
-    gap: 25px;
-    scrollbar-width: 0;
-    padding-bottom: 20px;
-    padding-top: 10px;
-    min-height: 400px;
-  }
+.popular-recipes {
+  overflow-x: scroll;
+  display: flex;
+  gap: 25px;
+  scrollbar-width: 0;
+  padding-bottom: 20px;
+  padding-top: 10px;
+  min-height: 400px;
+}
 
-  .popular-recipes::-webkit-scrollbar {
-    display: none;
-  }
+.popular-recipes::-webkit-scrollbar {
+  display: none;
+}
 
-  .recently-viewed-container {
-    margin-top: 50px;
-    margin-bottom: 30px;
-  }
+.recently-viewed-container {
+  margin-top: 50px;
+  margin-bottom: 30px;
+}
 
-  .recently-viewed-container h2 {
-    margin-bottom: 30px;
-  }
+.recently-viewed-container h2 {
+  margin-bottom: 30px;
+}
 
-  .recently-viewed {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    gap: 20px;
-  }
+.recently-viewed {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+}
 </style>
